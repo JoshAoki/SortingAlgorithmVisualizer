b63e2c0738703309e6dd615dcdfc7359adde73ce
JoshAoki <73375352+JoshAoki@users.noreply.github.com>
2021-06-15 21:31:13 -0500

    Bubble sort and quicksort work fully

--- visualizer.js
+++ visualizer.js
@@ -3,6 +3,8 @@ let i = 0;
 let w = 10;
 let states = [];
 
+let x = 0;
+
 let button;
 let btnClear;
 
@@ -10,12 +12,14 @@ let callMethod = true;
 //we only want quicksort to run once
 let drawBubbleSort = false;
 let drawQuickSort = false;
+let drawMergeSort = false;
 
 let checkRun = false;
 let titleBool = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  title();
   values = new Array(floor(width/w));
   for (let i = 0; i < values.length; i++) {
     values[i] = random(height);
@@ -34,9 +38,14 @@ function setup() {
   btnQuickSort.position(990,25);
   btnQuickSort.mousePressed(changeQuickBool);
 
+  btnQuickSort = createButton('Merge Sort');
+  btnQuickSort.size(100,40);
+  btnQuickSort.position(1130,25);
+  btnQuickSort.mousePressed(changeMergeBool);
+
   btnReset = createButton('Reset');
   btnReset.size(80);
-  btnReset.position(1130,35);
+  btnReset.position(1290,35);
   btnReset.mousePressed(reset);  
 }
 
@@ -50,33 +59,48 @@ function changeBool() {
   }
 }
 
-function changeQuickBool() {
-  
+function changeMergeBool() {
   if(checkRun==true) {
-    drawQuickSort = true;
+    drawMergeSort = true;
     loop();
   }
   else {
+    drawMergeSort = true;
+  }
+}
+
+function changeQuickBool() {
+  
+  if(checkRun==true&&drawBubbleSort==false) {
+    drawQuickSort = true;
+    loop();
+  }
+  else if(drawBubbleSort==false) {
     drawQuickSort = true;
   } 
 }
 
 function reset() {
-  i = 0;
-  drawBubbleSort = false;
-  drawQuickSort = false;
-  callMethod = true;
-  clear();
-  values = new Array(floor(width/w));
-  for (let i = 0; i < values.length; i++) {
-    values[i] = random(height);
+  if(isSorted(values)||(drawBubbleSort==false&&drawQuickSort==false&&drawQuickSort==false))
+  {
+    i = 0;
+    x = 0;
+    drawBubbleSort = false;
+    drawQuickSort = false;
+    drawMergeSort = false;
+    callMethod = true;
+    clear();
+  
+    values = new Array(floor(width/w));
+    for (let i = 0; i < values.length; i++) {
+      values[i] = random(height);
+    }
+    drawArray();
   }
-  drawArray();
 }
 
 function draw() {
-
-if(drawBubbleSort == true) {
+if(drawBubbleSort == true&&drawQuickSort==false) {
   background(255);
   if (i < values.length) {
     stroke(0);
@@ -87,12 +111,16 @@ if(drawBubbleSort == true) {
     }
     //This for loop runs once through the length, then iterates i by one after it breaks out then does in a loop until the entire sort is accounted for
   } else {
-    console.log('Done Sorting');
-    noLoop();
     checkRun = true;
+    console.log('Done Sorting');
+    
   }
   i++;
   drawArray();
+  if(isSorted(values)){
+    drawFinishedArray(x);
+    x++;
+  }
 }
 else if (drawQuickSort==true) {
   if(callMethod){
@@ -100,8 +128,21 @@ else if (drawQuickSort==true) {
     callMethod = false;
   }
   drawArray();
+  if(isSorted(values)){
+    drawFinishedArray(x);
+    x++;
+  }
+  }
+  
 }
 
+function isSorted(arr) {
+  for(let i = 0; i < arr.length-1; i++) {
+    if(arr[i] > arr[i+1]) {
+       return false;
+    }
+  }
+  return true;
 }
 
 function swap(arr, a, b) {
@@ -118,10 +159,26 @@ function title() {
   text('Sorting Algorithm Visualization',80, 50);
 }
 
-function drawArray() {
-  frameRate(10);
+
+function drawFinishedArray(x) {
+  frameRate(60);
   background(0);
-  for (let i = 0; i < values.length; i++) {
+  for (let i = 0; i < values.length; i++) {  
+    if(i<x) {
+      fill('#00ff00');
+    }
+    else{
+      fill(255);
+    }
+    rect(i * w , height-values[i], w ,values[i]);
+    title();
+  }
+}
+
+function drawArray() {
+  frameRate(20);
+  background(0);
+  for (let i = 0; i < values.length; i++) {   
     fill(255);
     if(drawQuickSort==true){
       if (states[i] == 0) {
@@ -141,6 +198,7 @@ async function quickSort(arr, start, end) {
   if (start >= end) {
     return;
   }
+  
   let index = await partition(arr, start, end);
   states[index] = -1;
 
@@ -148,6 +206,8 @@ async function quickSort(arr, start, end) {
     quickSort(arr, start, index - 1),
     quickSort(arr, index + 1, end)
   ]);
+
+  
 }
 
 async function partition(arr, start, end) {
@@ -165,6 +225,7 @@ async function partition(arr, start, end) {
       pivotIndex++;
       states[pivotIndex] = 0;
     }
+    
   }
   await quickSwap(arr, pivotIndex, end);
 
@@ -176,7 +237,7 @@ async function partition(arr, start, end) {
   return pivotIndex;
 }
 async function quickSwap(arr, a, b) {
-  await sleep(50);
+  await sleep(30);
   let temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
